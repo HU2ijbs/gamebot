fdd0e9734e249ea2f71d23147b8348b3eef5d2d2
HU2ijbs <gitpanel@atomicmail.io>
2026-08-08 18:33:42 +0330

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ define('BOT_TOKEN', '8735468401:AAHCTktkWFyQsIojCDGTtWKRU7WFGMA2iLk');
 define('TELEGRAM_API', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
 
 define('DB_HOST', 'mysql.railway.internal');
+define('DB_PORT', '3306');
 define('DB_NAME', 'railway');
 define('DB_USER', 'root');
 define('DB_PASS', 'cDUvsQVNtkLRztHyaFWAPaJWTAIUDPSx');
