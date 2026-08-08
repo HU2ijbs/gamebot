bb0e768b765d0cdea01ff9960bd64ebf3c716d3d
HU2ijbs <gitpanel@atomicmail.io>
2026-08-08 18:04:05 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('BOT_TOKEN', '8735468401:AAHCTktkWFyQsIojCDGTtWKRU7WFGMA2iLk');
-define('TELEGRAM_API', 'https://api.telegram.org/bot' . 8735468401:AAHCTktkWFyQsIojCDGTtWKRU7WFGMA2iLk . '/');
+define('TELEGRAM_API', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
 
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'dbName');
@@ -11,7 +11,7 @@ define('DB_CHARSET', 'utf8mb4');
 
 define('ADMIN_IDS', [7727625618]);
 
-define('@HusteRIX', '7727625618'); 
+// define('HusteRIX', '7727625618'); // اگر نیاز داری
 
 define('START_GOLD', 5000);
 define('START_OIL', 1000);
@@ -20,11 +20,11 @@ define('START_ECONOMY', 100);
 define('START_TECH', 100);
 define('START_POPULATION', 100000);
 
-define('FREE_MONEY_PER_PIP', 50); 
-define('FREE_MONEY_COOLDOWN_MIN', 1440); // 24 saat
+define('FREE_MONEY_PER_PIP', 50);
+define('FREE_MONEY_COOLDOWN_MIN', 1440); // 24 ساعت
 
-define('NUKE_TECH_REQUIREMENT', 500); 
-define('NUKE_GOLD_COST', 20000); 
+define('NUKE_TECH_REQUIREMENT', 500);
+define('NUKE_GOLD_COST', 20000);
 define('NUKE_BOMB_GOLD_COST', 150000);
 define('NUKE_MIN_BOMBS_PER_ATTACK', 5);
 
@@ -42,7 +42,7 @@ define('CHANNEL_CHECK_CACHE_MIN', 10);
 define('CHANNEL_CHECK_NEGATIVE_CACHE_SEC', 20);
 define('DB_PERSISTENT', true);
 
-define('ALLIANCE_CREATE_COST', 20000); 
+define('ALLIANCE_CREATE_COST', 20000);
 
 define('PEACE_TREATY_DAYS', 3);
 define('PEACE_BREAK_GOLD_COST', 5000);
@@ -50,8 +50,8 @@ define('PEACE_BREAK_REPUTATION_PENALTY', 15);
 
 define('SPY_EQUIPMENT_COSTS', [1 => 1000, 2 => 3000, 3 => 6000]);
 define('COUNTER_SPY_EQUIPMENT_COSTS', [1 => 1200, 2 => 3500, 3 => 7000]);
-define('SPY_EQUIPMENT_BONUS_PERCENT', 15); 
-define('COUNTER_SPY_EQUIPMENT_BONUS_PERCENT', 15); 
+define('SPY_EQUIPMENT_BONUS_PERCENT', 15);
+define('COUNTER_SPY_EQUIPMENT_BONUS_PERCENT', 15);
 define('SPY_BASE_SUCCESS_PERCENT', 55);
 define('SPY_COST_GOLD', 500);
 
